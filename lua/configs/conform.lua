f63e23358c68a9016248d525e90a1ede6d443046
Richard Habitzreuter <richardhabitzreuter@icloud.com>
2024-07-18 19:14:01 -0300

    Update conform.lua

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,6 +1,8 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
+    rust = { "rust_analyzer" },
+    typescript = { "prettier" },
     -- css = { "prettier" },
     -- html = { "prettier" },
   },
